9051bad835b8e8902a5e390e4dd606fb9d667a5a
Kawtar Aboussi <kautar.abussi@gmail.com>
2023-06-24 13:31:23 +0100

    sort vars in export && fixed (+ && =)

--- execution/utils.c
+++ execution/utils.c
@@ -6,21 +6,26 @@
 /*   By: kaboussi <kaboussi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/16 15:03:37 by kaboussi          #+#    #+#             */
-/*   Updated: 2023/06/22 14:06:30 by kaboussi         ###   ########.fr       */
+/*   Updated: 2023/06/24 11:21:53 by kaboussi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-int	ft_strncmp(const char *str1, const char *str2, size_t n)
+int	ft_strncmp(char *st1, char *st2, size_t n)
 {
 	size_t			i;
-	unsigned char	*st1;
-	unsigned char	*st2;
 
 	i = 0;
-	st1 = (unsigned char *)str1;
-	st2 = (unsigned char *)str2;
+
+	if (st1 == NULL)
+	{
+		return (-1);
+	}
+	else if (st2 == NULL)
+	{
+		return (1);
+	}
 	while ((st1[i] != '\0' || st2[i] != '\0') && i < n)
 	{
 		if (st1[i] != st2[i])
