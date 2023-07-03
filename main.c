18e7c0798e9cc42edddd593aebce524d461c20c1
ysahih <ysahih@student.1337.ma>
2023-07-03 15:18:42 +0100

    FIXED : merging words

--- main.c
+++ main.c
@@ -342,10 +342,10 @@
 void	handle_INT(int sig)
 {
 	(void)sig;
-	// rl_catch_signals = 0;
+	rl_catch_signals = 0;
 	printf("\n");
 	rl_on_new_line();
-	// rl_replace_line("", 0);
+	rl_replace_line("", 0);
 	rl_redisplay();
 }
 
@@ -396,44 +396,35 @@ int	main(int ac, char **av, char **env)
 	i = 0;
 	if (ac != 1 || av[1])
 		return (printf("program does not accept agruments"), 0);
-	// all = malloc(sizeof(t_all));
 
-	// all.env = NULL;
-	// all.exp = NULL;
-	// if (!*env)
-	// {
-	// 	add_exen_back(&all.exp ,lstnew_exen(ft_strdup("OLDPWD"), NULL));
-	// 	add_exen_back(&all.exp ,lstnew_exen(ft_strdup("PWD"), ft_strdup(getcwd(path, 800))));
-	// 	add_exen_back(&all.exp ,lstnew_exen(ft_strdup("SHLVL"), ft_strdup("1")));
-	// 	add_exen_back(&all.env ,lstnew_exen(ft_strdup("PWD"), ft_strdup(getcwd(path, 800))));
-	// 	add_exen_back(&all.env ,lstnew_exen(ft_strdup("SHLVL"), ft_strdup("1")));
-	// }
-	// while (env[i])
-	// {
-	// 	lst_var(&all.env, ft_split(env[i]));
-	// 	lst_var(&all.exp, ft_split(env[i]));
-	// 	i++;
-	// }
 	set_env(&all, env);
 	while (true)
 	{
 		sig_handler();
-		// rl_catch_signals = 0;
+		rl_catch_signals = 0;
 		line = readline("sash$ ");
-		// printf("line ...... {%s}\n", line);
-		// line = NULL;
-		// printf("%s\n", line);
 		if (!line)
 			break ;
+		if (*line)
+			add_history(line);
 		cmd = tokenize(line);
+
+		// // puts("hehe");
+		// t_lexer *tmp = cmd;
+		// while (tmp)
+		// {
+		// 	// w = (*node)->str;
+		// 	printf("%d\n", tmp->type);
+		// 	tmp = tmp->next;
+		// }
+
+
 		if (!analyze_syntax(cmd))
 		{
 			write (1, "syntax error\n", 13);
 			continue ;
 		}
 		parse(&all, cmd);
-		if (*line)
-			add_history(line);
 		exec(&all);
 	}
 }
